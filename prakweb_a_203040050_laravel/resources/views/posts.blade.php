19bce8caa57dfe5c11120f48c21e2b275a519af9
Hafiszah Saidatul <203040050@mail.unpas.ac.id>
2022-10-27 06:28:27 +0700

    Menyelesaikan Video 7 Post Model

--- prakweb_a_203040050_laravel/resources/views/posts.blade.php
+++ prakweb_a_203040050_laravel/resources/views/posts.blade.php
@@ -5,9 +5,10 @@
 @foreach ($posts as $post)
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a></h2>
-        <h5>by: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+        <!-- <h5>{{ $post["author"] }}</h5> -->
+        <p>{{ $post->excerpt }}</p>
+        <!-- <p>{{ $post["body"] }}</p> -->
     </article>
 @endforeach
 
