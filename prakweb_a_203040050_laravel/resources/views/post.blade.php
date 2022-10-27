1622f716e463ef9c0983b6ba0b3c34d0b7c739ad
Hafiszah Saidatul <203040050@mail.unpas.ac.id>
2022-10-27 20:51:14 +0700

    Menyelesaikan Video 8 Category & Relationship

--- prakweb_a_203040050_laravel/resources/views/post.blade.php
+++ prakweb_a_203040050_laravel/resources/views/post.blade.php
@@ -2,8 +2,8 @@
 
 @section('container')
 <article>
-    <h2>{{ $post->title }}</h2>
-    <!-- <h5>{{ $post["author"] }}</h5> -->
+    <h2 class="mb-5">{{ $post->title }}</h2>
+   <p>By. Hafiszah Saidatul in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name   }}</a></p>
     {!! $post->body  !!}
 </article>
 <a href="/blog">Back to posts</a>
