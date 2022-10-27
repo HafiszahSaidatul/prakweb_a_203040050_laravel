8e87ebafd22a663ffca20e7f6ba546ac7ea77f72
Hafiszah Saidatul <203040050@mail.unpas.ac.id>
2022-10-27 22:06:43 +0700

    Menyelesaikan Video 9 Database Seeder

--- prakweb_a_203040050_laravel/app/Models/User.php
+++ prakweb_a_203040050_laravel/app/Models/User.php
@@ -38,7 +38,14 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
