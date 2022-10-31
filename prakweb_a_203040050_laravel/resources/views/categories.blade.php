38ce719e272325e7ad62c8c7f23a683f9175d982
Hafiszah Saidatul <203040050@mail.unpas.ac.id>
2022-10-31 23:45:04 +0700

    Menyelesaikan Video 13. Searching & Pegination

--- prakweb_a_203040050_laravel/resources/views/categories.blade.php
+++ prakweb_a_203040050_laravel/resources/views/categories.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             @foreach ($categories as $category)
             <div class="col-md-4">
-                <a href="/categories/{{ $category->slug }}">
+                <a href="/blog?category={{ $category->slug }}">
                 <div class="card bg-dark text-white">
                 <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name}}">
                 <div class="card-img-overlay d-flex align-items-center p-0">
@@ -20,17 +20,4 @@
             @endforeach
         </div>
     </div>
-
-    @foreach ($categories as $category)
-
-        <!-- <ul>
-            <li>
-            <h2>
-            <a href="/categories/{{ $category->slug }}">{{ $category->name}}</a>
-            </h2>       
-            </li>
-        </ul> -->
-
-
-    @endforeach
 @endsection
--- prakweb_a_203040050_laravel/routes/web.php
+++ prakweb_a_203040050_laravel/routes/web.php
@@ -19,13 +19,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home' , [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => 'about',
         "name" => "Hafiszah Saidatul",
         "email" => "hafiszahsaidatul@gmail.com",
         "image" => "kaws.jpg"
@@ -43,19 +45,7 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => 'categories',
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+
+
 
